cfb025fa999aa15b416df70fb3bcb569e3074f30
rnick1 <rnick0260@gmail.com>
2021-03-30 21:04:30 -0700

    Finished first draft of user, group, rule, and event models.

--- models/Event.js
+++ models/Event.js
@@ -1,12 +1,7 @@
 const { Model, DataTypes } = require('sequelize');
-const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
-class Event extends Model {
-  checkPassword(loginPw) {
-    return bcrypt.compareSync(loginPw, this.password);
-  }
-}
+class Event extends Model {}
 
 Event.init(
   {
