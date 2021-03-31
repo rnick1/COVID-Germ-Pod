425a932fb4c9c4d55cf1768f83ce259fecb2fa2e
Ingrid Lyublinsky <ingrid.lyub@gmail.com>
2021-03-30 19:34:30 -0700

    started server.js file

---
+++ server.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+
+
+const sequelize = require('./config/connection');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 4040;
+
+const hbs = exphbs.create({ helpers });
+
+// Session to sequelize store
+const sess = {
+  secret: 'secret pod squad',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+// express session & handlebars
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'pod_squad')));
+
+app.use(routes);
+
+//port connection
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
