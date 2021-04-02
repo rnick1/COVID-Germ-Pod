7e706d48d92ee2c209b2dbeda29c1bb77e3e5ead
Mikey P <peraram2@gmail.com>
2021-04-01 19:02:09 -0700

    login form handler created

---
+++ public/js/login.js
@@ -0,0 +1,21 @@
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('password-login').value.trim();
+
+    if(email && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({ email, password }),
+            headers: {'Content-Type': 'application/json' },
+        });
+
+        if(response.ok) {
+            // if successful, redirect to profile page
+            document.location.replace('/profile');
+        } else {
+            alert(response.statusText)
+        }
+    }
+}
