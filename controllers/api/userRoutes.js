43a7e2d5138296a274657ec47eeb71465229d9b6
noheropage <mursepage@gmail.com>
2021-04-04 15:46:39 -0700

    Refactor login logic, add profile destination, fix pw encryption

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const {User} = require('../../models');
+const { User } = require('../../models');
 
 router.post('/', async (req, res) => {
     try{
@@ -33,7 +33,7 @@ router.post('/login', async (req, res) => {
 
         req.session.save(() => {
             req.session.user_id = userData.id;
-            req.session.loged_in = true;
+            req.session.logged_in = true;
 
             res.json({ user: userData, message: 'You are now logged in!'})
         })
