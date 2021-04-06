c39be95c5fe54120e5f1f8324f96e0db46aa6b5f
noheropage <mursepage@gmail.com>
2021-04-06 00:28:21 -0700

    Change seed values in userEvent and groupRule from string into ints

--- seeds/userEvent-seeds.js
+++ seeds/userEvent-seeds.js
@@ -2,16 +2,16 @@ const { UserEvent } = require('../models');
 
 const userEventData = [
     {
-        "user_id": "1",
-        "event_id": "1",
+        "user_id": 1,
+        "event_id": 1,
     },
     {
-        "user_id": "1",
-        "event_id": "2",
+        "user_id": 1,
+        "event_id": 2,
     },
     {
-        "user_id": "2",
-        "event_id": "3",
+        "user_id": 2,
+        "event_id": 3,
     },
 ]
 
